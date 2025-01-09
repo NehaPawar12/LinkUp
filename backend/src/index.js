5fca7b6ef6503dc8fa3ed20b2f5eb299d3e9707e
Neha Pawar <nehapawar2320@gmail.com>
2025-01-09 17:46:17 +0530

    Database setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import dotenv from 'dotenv'
+import { connect } from "mongoose";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
+dotenv.config()
+
 app.use("/api/auth", authRoutes)
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    //MongoDB Connection
+    connectDB()
 })
 
 //Express is the 1st package(web frame work) we are using that will provide us with routes middlewares etc.
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+//Creating Schema
+const userSchema = new mongoose.Schema(
+    {
+        //Putting in all the fields for the user 
+        email: {
+            type:String,
+            required: true,
+            unique: true,
+        },
+
+        fullname: {
+            type :String,
+            required : true,
+        },
+
+        password : {
+            type : String,
+            required: true,
+            minlength: 6,
+        },
+
+        profilePic: {
+            type: String,
+            default: "",
+        }
+    },
+
+    {timestamps: true}
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User; //Exporting it as to use in other files.
