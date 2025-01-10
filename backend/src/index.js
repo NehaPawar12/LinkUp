36d1d11bfab61144887dc459a01d9543a57b7eec
Neha Pawar <nehapawar2320@gmail.com>
2025-01-10 20:36:10 +0530

    Configuring the message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv'
 import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
@@ -13,6 +14,7 @@ app.use(express.json()) // Will allow to extract the json data out of body
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 const PORT = process.env.PORT
 
