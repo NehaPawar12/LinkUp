e2c380d701809f05bbfe7c6d7cc436dd210a23e0
Neha Pawar <nehapawar2320@gmail.com>
2025-01-11 19:35:22 +0530

    setting up the pages visible to users that are signed up

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 const app = express();
 
@@ -15,6 +16,11 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+app.use(cors({ 
+    origin: "http://localhost:5173",
+    credentials: true
+}
+))
 
 const PORT = process.env.PORT
 
