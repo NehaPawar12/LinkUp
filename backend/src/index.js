5804baad7fd827f5578d522eed7ca8abe4cfc566
Neha Pawar <nehapawar2320@gmail.com>
2025-01-12 23:09:58 +0530

    Preparing for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv'
-//import { connect } from "mongoose";
+import path from 'path'
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
@@ -13,6 +13,7 @@ dotenv.config()
 
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(express.json()) // Will allow to extract the json data out of body
 app.use(cookieParser())
@@ -25,7 +26,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
