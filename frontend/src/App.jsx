6c43bf5089fefaee9e771bfc657d05d503b1516c
Neha Pawar <nehapawar2320@gmail.com>
2025-01-12 13:15:19 +0530

    Working on the settings page to let the use apply different themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,13 @@ import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore'
+import {useThemeStore} from './store/useThemeStore'
 import {Loader} from 'lucide-react'
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const {theme} = useThemeStore();
 
   useEffect(() => {
     checkAuth()
@@ -23,7 +25,7 @@ const App = () => {
     <div className='flex items-center justify-center h-screen'><Loader className='size-10 animate-spin'/></div>
   )
   return (
-    <div>
+    <div data-theme={theme}>
 
       <Navbar/>
       <Routes>
